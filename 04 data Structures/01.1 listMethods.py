0e1aaab2769bb609c64176a8860896cc5a80eaba
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-03-07 03:25:25 +0500

    list methods

---
+++ 04 data Structures/01.1 listMethods.py
@@ -0,0 +1,25 @@
+#list.append()
+list1=[1,2,3]
+list1.append(4)
+print(list1)
+#list.pop()
+list1.pop()
+print(list1)
+#list.remove()
+list1.remove(1) 
+print(list1)
+#list.insert()
+list1.insert(0,5) #insert 5 at index 0
+print(list1)
+#list.extend()
+list2=[6,7,8]
+list1.extend(list2) #appends list2 to list1
+print(list1)
+#list.sort()
+list1.sort()
+print(list1)
+#list.reverse()
+list1.reverse()
+print(list1)
+list1.clear() #empties the entire list
+print(list1) 
