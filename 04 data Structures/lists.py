5d7bc6b492f31280a5b7e6c5c2d50df608a5d010
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-03-07 03:00:23 +0500

    data structor list introduced

---
+++ 04 data Structures/lists.py
@@ -0,0 +1,10 @@
+# list are mutable indexed collection of elements denoted by []
+# the elements can have different data types ,int,float,string,,,,
+list1=[1,2,3,4,5,6]
+list2=['Ubaid',123,4.7,'student']
+
+print(list1,list2)
+print(type(list1),type(list2)) # both should be class list
+
+#we can slice a list as string
+print(list1[2:4]) #start from index 2 and stop before index 4 
