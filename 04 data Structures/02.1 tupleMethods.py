3cc33ebaf6e8703f8c941b7f92600ca57c3ddd22
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-03-08 13:46:43 +0500

    tuple methods

--- 04 data Structures/02.1 tupleMethods.py
+++ 04 data Structures/02.1 tupleMethods.py
@@ -1,5 +1,5 @@
 #tuples are mutable
-#so do not have mush methods like lists
+#so do not have much methods like lists
 
 #1 count(x) we can count appearance of an element in a tuple
 tuple1=(1,2,1,2,3,4,3,3,4,3)
@@ -16,3 +16,12 @@ print(tuple2) # now we have 5 at index 0
 
 # but the original tuple1 remains the same
 print(tuple1)
+
+# tuple3=tuple1.append(tuple2)  ❌ woud'nt work
+#we need to convrt both tuples into list first
+tuplex=(10,11,'Ubaid',90,45.5)
+lista=list(tuple1)
+listb=list(tuplex)
+lista.extend(listb)
+tuplec=tuple(lista)
+print(tuplec)
