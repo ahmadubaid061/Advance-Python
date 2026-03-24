b77cc075510fd8470f366f0eb4eee0996720af35
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-03-24 06:38:44 +0500

    inheretence and polymorphism

---
+++ 05 OOP in Python/04 inheretence.py
@@ -0,0 +1,5 @@
+#inheretence is what classes  classes are used for 
+#due to inheretence we use methods of one class in other classes (child classes)
+#Inheretence means passing of something from parents to offsprings
+#in programming that something is the attributes and methods
+
---
+++ 05 OOP in Python/05 polymorphism.py
@@ -0,0 +1,4 @@
+#poly means many and morphism means forms 
+'''polymorphism means that a single class method can have ,multiple (in different forms) 
+ implementation based on the context where they are used
+ '''
