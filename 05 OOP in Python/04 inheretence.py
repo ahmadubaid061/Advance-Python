173de420a6cec26b80fd8a1d7f4ac41acc658598
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-04-24 10:34:15 +0500

    Inheretence in python OOP

--- 05 OOP in Python/04 inheretence.py
+++ 05 OOP in Python/04 inheretence.py
@@ -3,3 +3,35 @@
 #Inheretence means passing of something from parents to offsprings
 #in programming that something is the attributes and methods
 
+#suppose we have a class called Animal and it has some attributes and methods and we want to use those attributes and methods in other classes like Dog and Cat then we can use inheretence
+class Animal:
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+
+    def make_sound(self):
+        if self.species=="Dog":
+            return "Woof"
+        elif self.species=="Cat":
+            return "Meow"
+        else:
+            return "Unknown sound"
+
+#now we can create classes for Dog and Cat that inherit from the Animal class
+class Dog(Animal):
+    def __init__(self,name):
+        super().__init__(name,"Dog")
+
+class Cat(Animal):
+    def __init__(self,name):
+        super().__init__(name,"Cat")    
+    
+#now we can create objects of the Dog and Cat classes and use the make_sound method which is inherited from the Animal class
+dog1=Dog("Buddy")   
+cat1=Cat("Whiskers")
+print(dog1.name) #this will print Buddy
+print(dog1.species) #this will print Dog
+print(dog1.make_sound()) #this will print Woof
+print(cat1.name) #this will print Whiskers
+print(cat1.species) #this will print Cat
+print(cat1.make_sound()) #this will print Meow
